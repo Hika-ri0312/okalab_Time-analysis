2f49daee4805d4f1f2280599e00df368a66faa4a
Hikari Ishihara <e205217@ie.u-ryukyu.ac.jp>
2023-02-10 13:46:06 +0900

    column_setthings_fixed

--- chap1/1-5/1_5_act.py
+++ chap1/1-5/1_5_act.py
@@ -1,24 +1,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import data_fix
 
-#データ確認
-df_historical = pd.read_csv('m_quote_fix.csv', encoding="shift-jis")
-# ここで、手動でカラム名'data'と付けた
+def fixed_data_index():
+    #データ確認
+    df_historical = pd.read_csv('m_quote_fix.csv', encoding="shift-jis")
+    # ここで、手動でカラム名'data'と付けた
 
-#カラム'data'をインデックスに設定
-df_i = df_historical.set_index('data')
+    #カラム'data'をインデックスに設定
+    # df_i = df_historical.set_index('data')
+    df_i = df_historical.rename(columns={'':'data'})
+    return df_i
 
-df_i.drop(columns=['Unnamed: 0'], inplace=True)
-print(df_i)
+def main():
+    data_fix.datafile_fixed()
+    df_i = fixed_data_index()
+    #24ヶ月移動平均
+    df_i_24 = df_i.USD.rolling(window=24).mean()
+    #48ヶ月移動平均
+    df_i_48 = df_i.USD.rolling(window=48).mean()
 
-#24ヶ月移動平均
-df_i_24 = df_i.USD.rolling(window=24).mean()
-#48ヶ月移動平均
-df_i_48 = df_i.USD.rolling(window=48).mean()
-
-df_i.USD.plot()
-df_i_24.plot()
-df_i_48.plot()
-plt.savefig("mean_3data.png")
-plt.show()
+    df_i.USD.plot()
+    df_i_24.plot()
+    df_i_48.plot()
+    plt.savefig("mean_3data.png")
+    plt.show()
+    
+if __name__ == "__main__":
+    main()
